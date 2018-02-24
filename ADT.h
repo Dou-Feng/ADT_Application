99d8b2d92914d5d5efafe95e3f7be77e9baa6b17
Troubledesigner <821878167@qq.com>
2018-02-24 17:15:28 +0800

    finish ADT operation

--- ADT.h
+++ ADT.h
@@ -1,8 +1,29 @@
 #pragma once
+#include "AVLTree.h"
+
+
 class ADT
 {
 public:
 	ADT();
-	
+	void getAllElem(Member *coll);
+	int getSize();
+	bool insert(Member coll);
+	int insert(Member *coll, int size);
+	int insert(ADT *A);
+	bool remove(Member m);
+	int remove(Member *m, int size);
+	int remove(ADT *A);
+	bool destroy();
+
+	static ADT *set_intersection(Member *coll, ADT *a, ADT *b);
+	static ADT *set_different(Member *coll, ADT *a, ADT *b);
+	static ADT *set_union(Member *coll, ADT *a, ADT *b);
+	static bool set_subset(ADT *a, ADT *b);
+	static bool set_equal(ADT *a, ADT *b);
+
+public:
+	AVLTree *elems;
+
 };
 
